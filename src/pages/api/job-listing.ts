982cb689bdb913fe31c831af3bc5e06dcf8f73bc
ntwari-egide <ntwariegide2@gmail.com>
2024-05-24 14:42:23 -0400

    feat: fix all jobs requests

--- src/pages/api/job-listing.ts
+++ src/pages/api/job-listing.ts
@@ -11,27 +11,34 @@ export default async function handler(
     return res.status(405).json({ message: 'Only GET requests allowed' });
   }
 
-  const { title, company, companyLocation } = req.body;
+  const allJobs = [];
 
-  const options = {
-    method: 'GET',
-    url: 'https://jobs-api14.p.rapidapi.com/list',
-    params: {
-      query: `${title}, ${company}`,
-      location: companyLocation,
-      language: 'en_GB',
-    },
-    headers: {
-      'X-RapidAPI-Key': 'c48dc9f7a6msh4210fd7179433c2p115b0ajsn901ff271c00a',
-      'X-RapidAPI-Host': 'jobs-api14.p.rapidapi.com',
-    },
-  };
+  const { title, companyMatches } = req.body;
+  
+  // loop in all companies provided in req.body and send the request to the rapidapi endpoint and map the response to the company
 
-  try {
-    const response = await axios.request(options);
+  for(const company of companyMatches || []) {
+    const options = {
+      method: 'GET',
+      url: 'https://jobs-api14.p.rapidapi.com/list',
+      params: {
+        query: `${title}, ${company.name}`,
+        location: company.companyLocation,
+        language: 'en_GB',
+      },
+      headers: {
+        'X-RapidAPI-Key': 'c48dc9f7a6msh4210fd7179433c2p115b0ajsn901ff271c00a',
+        'X-RapidAPI-Host': 'jobs-api14.p.rapidapi.com',
+      },
+    };
 
-    res.status(200).json(response.data);
-  } catch (error) {
-    res.status(500).json({ message: 'Error processing the request' });
+    try {
+      const response = await axios.request(options);
+      allJobs.push(response.data);
+    } catch (error) {
+      res.status(500).json({ message: 'Error processing the request' });
+    }
   }
+
+  res.status(200).json(allJobs);
 }
