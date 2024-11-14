22487f038b0442c387bdc35309f9c681a42daea6
ntwari-egide <ntwariegide2@gmail.com>
2024-11-14 17:06:13 -0500

    feat: implement resume scanning routing

--- src/component/controls/file-input.tsx
+++ src/component/controls/file-input.tsx
@@ -1,7 +1,6 @@
 /* eslint-disable unused-imports/no-unused-vars */
-import { LoadingOutlined } from '@ant-design/icons';
 import { AnyAction, ThunkDispatch } from '@reduxjs/toolkit';
-import { Flex, message, Modal, Progress, Spin } from 'antd';
+import { Flex, message } from 'antd';
 import Image from 'next/image';
 import { useRouter } from 'next/router';
 import { useRef, useState } from 'react';
@@ -9,18 +8,13 @@ import { useDispatch, useSelector } from 'react-redux';
 
 import { RootState } from '@/store';
 
-import { jobListingRequest } from '@/features/job-listing';
-import { analyzeResume } from '@/features/open-ai';
 import { uploadFile } from '@/features/resume-scanner';
-import {
-  setJobListing,
-  setOpenAi,
-  setResumeScanner,
-} from '@/features/tracking-progress';
 
 import TextButton from './text-button';
 import TypingAnimation from '../reusable/typing-animations';
 import AIIcon from '../../../public/images/ai-icon.png';
+import { encryptData } from '@/utils/encryptions';
+import Cookies from 'js-cookie';
 
 type ReusableFileInputProps = {
   onChange?: (file: File) => void;
@@ -38,13 +32,9 @@ const ReusableFileInput = ({
   style,
   buttonContent,
 }: ReusableFileInputProps) => {
-  const [loading, setLoading] = useState(false);
-  const [imageUrl, setImageUrl] = useState<string | undefined>();
   const Router = useRouter();
   const fileInputRef = useRef<HTMLInputElement>(null);
-  const [isRedirected, setIsRedirected] = useState(false);
 
-  const [openResultsModel, setOpenResultsModel] = useState(false);
 
   const dispatch: ThunkDispatch<RootState, null, AnyAction> = useDispatch();
   const { error, response } = useSelector(
@@ -65,44 +55,24 @@ const ReusableFileInput = ({
     const file = event.target.files?.[0];
     if (!file) return;
 
-    setOpenResultsModel(true); // make the progress bar visible
-    setLoading(true);
-
     const formData = new FormData();
     formData.append('file', file);
 
     try {
-      await dispatch(setResumeScanner('STARTED'));
       const resumeText = await dispatch(uploadFile(formData)).unwrap();
-      await dispatch(setResumeScanner('COMPLETED'));
 
-      // getting the company matches using open ai api
-      await dispatch(setOpenAi('STARTED'));
-      const companyMatches = await dispatch(
-        analyzeResume(resumeText.content)
-      ).unwrap();
-      await dispatch(setOpenAi('COMPLETED'));
+      // set this encrypted format resume content to the cookies
+      Cookies.set(
+        'resume-content',
+        await encryptData(JSON.stringify(resumeText)),
+        { expires: 7 }
+      );
 
-      // get company matches from the response
-      await dispatch(setJobListing('STARTED'));
-      await dispatch(jobListingRequest(companyMatches));
-      await dispatch(setJobListing('COMPLETED'));
-
-      // // send the route to the /response and open in new tab
-      // if (validity && validity?.isValid) {
-      //   Router.push('/response');
-      // } else {
-      //   message.error('Invalid resume');
-      // }
-
-      // Get this url from response in real world.
-      getBase64(file, (url) => {
-        setLoading(false);
-        setImageUrl(url);
-      });
+      // router to the scanning page
+      Router.push("/scanning")
+      
     } catch (error) {
       message.error('Error processing the file');
-      setLoading(false);
     }
   };
 
@@ -155,120 +125,6 @@ const ReusableFileInput = ({
           />
         </div>
       </Flex>
-
-      {/* Response Layout Modal component */}
-
-      <Modal
-        open={openResultsModel}
-        className='w-[60vw] relative'
-        onCancel={() => setOpenResultsModel(false)}
-        footer={null}
-      >
-        {validity && !validity?.isValid ? (
-          <div>
-            <div className=' h-[60vh] text-[#000000]'>
-              {validity?.errors.map((error, index) => (
-                <>
-                  <p key={index} className='text-[1.5vh]'>
-                    {error.section}
-                  </p>
-                  <p key={index} className='text-[1.5vh]'>
-                    {error.message}
-                  </p>
-                </>
-              ))}
-            </div>
-          </div>
-        ) : (
-          <div>
-            <div className='w-full flex justify-center py-[5vh]'>
-              <div>
-                <div className='flex justify-center'>
-                  <Spin
-                    className='text-[#348687]'
-                    indicator={
-                      <LoadingOutlined
-                        style={{ fontSize: 130, color: '#348687' }}
-                        spin
-                      />
-                    }
-                  />
-                </div>
-                <div className='flex flex-col mt-[4vh] gap-[2vw] w-[40vw]'>
-                  <div className='flex flex-row gap-[1vw]'>
-                    <Progress
-                      percent={
-                        progress.resumeScaner === 'STARTED'
-                          ? 40
-                          : progress.resumeScaner === 'COMPLETED'
-                          ? 100
-                          : 0
-                      }
-                      size='small'
-                      status={
-                        progress.resumeScaner === 'COMPLETED'
-                          ? 'success'
-                          : 'active'
-                      }
-                    />
-                    <h1 className='text-[#000000] text-[2vh] w-1/2'>
-                      {progress.resumeScaner === 'STARTED'
-                        ? 'Extracting Data'
-                        : 'Data Extracted'}
-                    </h1>
-                  </div>
-
-                  <div className='flex flex-row gap-[1vw]'>
-                    <Progress
-                      percent={
-                        progress.resumeScaner === 'STARTED'
-                          ? 40
-                          : progress.resumeScaner === 'COMPLETED'
-                          ? 100
-                          : 0
-                      }
-                      size='small'
-                      status={
-                        progress.resumeScaner === 'COMPLETED'
-                          ? 'success'
-                          : 'active'
-                      }
-                    />
-                    <h1 className='text-[#000000] text-[2vh] w-1/2'>
-                      {progress.resumeScaner === 'STARTED'
-                        ? 'Extracting Data'
-                        : 'Data Extracted'}
-                    </h1>
-                  </div>
-
-                  <div className='flex flex-row gap-[1vw]'>
-                    <Progress
-                      percent={
-                        progress.jobListing === 'STARTED'
-                          ? 50
-                          : progress.jobListing === 'COMPLETED'
-                          ? 100
-                          : 0
-                      }
-                      size='small'
-                      status={
-                        progress.jobListing === 'COMPLETED'
-                          ? 'success'
-                          : 'active'
-                      }
-                    />
-                    <h1 className='text-[#000000] text-[2vh] w-1/2'>
-                      {progress.jobListing === 'STARTED'
-                        ? 'Matching Job Listings'
-                        : 'Listings Matched'}
-                    </h1>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        )}
-      </Modal>
     </>
   );
 };
